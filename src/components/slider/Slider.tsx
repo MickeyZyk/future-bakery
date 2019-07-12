4f9131f5869036c37a34021e1d3619527e913ec9
Dalibor Ilić <dalibor.ilic.dado@gmail.com>
2019-07-12 14:59:40 +0200

    Update Slider.tsx

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { TweenMax, TimelineMax } from "gsap";
+import { TweenMax, TimelineMax, Power2} from "gsap";
 import { Tween } from 'react-gsap';
 import logo from 'assets/svg/bakery-logo.svg';
 import ReactDOM from 'react-dom';
@@ -10,8 +10,12 @@ import ReactDOM from 'react-dom';
  */
 var startCarouselInterval;
 
-var images = ['../images/image.jpg', '../images/dude.jpg','../images/image.jpg', '../images/dude.jpg', '../images/image.jpg']
-var labels = ["Cats are Awesome 01", "Yes they are 02", "Yes they are 03","Yes they are 04", "Yes they are 05"  ]
+var images = ['../images/image.jpg', '../images/dude.jpg','../images/desk.jpg']
+var labels = ["Cats are Awesome 01", "Yes they are 02", "Yes they are 03" ]
+var slidesCount = images.length;
+var percentage = 0;
+var multiplier = 100 / images.length;
+console.log("percentage" + percentage);
 
 export class Slider extends React.Component {
   componentDidMount(){
@@ -25,94 +29,103 @@ export class Slider extends React.Component {
 }
 class Carousel extends React.Component {
   constructor(props){
-    super(props)
+    super(props);
+    this.wrapperRef = React.createRef();
+    this.childRef = React.createRef();    
     this.state ={
-      which:0,
-      showButtons:false
+      which: slidesCount,
+      showButtons: false,
+      forward: true,
+      firstSlide: false,
+      lastSlide: false
     };
+    this.titles = [];﻿
   }
 
   componentDidMount(){
+    //console.log(this.childRef.current.imageRefActive.current, "parent");
     if(this.props.auto){
       this.startCarousel()
-    }
+    };
     if(this.props.showButtons){
       this.setState({showButtons:true})
-    }
+    };
   }
 
   startCarousel(){
     startCarouselInterval  = setInterval(this.nextSlide.bind(this), this.props.timeInBetween);
   }
 
+
   prevSlide(){
 
-    //console.log(this.state.which + 1, images.length) 
+    console.log(this.state.which);
 
-    if(this.state.which !== 0){
-      this.setState({
-        which:--this.state.which
-      })
-    } else {
-      this.setState({
-        which:images.length - 1
-      })
-    }
+    percentage = this.state.forward ? percentage + multiplier : (this.state.which < slidesCount ? percentage + multiplier : multiplier);
+
+    var image = this.wrapperRef.current; 
+
+    var tl = new TimelineMax({repeat:0});
+    tl.to(image , 2 , { top: `${percentage}%`, ease: 'Expo.easeOut'});
+
+    console.log("PREV", "percentage", percentage, "this.state.which", this.state.which, "slidesCount", slidesCount, "this.state.forward", this.state.forward); 
+
+    this.setState({
+      which: this.state.which < slidesCount ? ++this.state.which : slidesCount,
+      forward: false 
+    })
+
   }
 
   nextSlide(){
 
-    //console.log(this.state.which + 1, images.length)
+    console.log(this.state.which);    
 
-    if(startCarouselInterval){
-      clearInterval(startCarouselInterval);
-     
-      setTimeout(() =>{
-         startCarouselInterval  = setInterval(this.nextSlide.bind(this), this.props.timeInBetween);
-      }, 500);
-    }
-        
-    if(this.state.which < images.length - 1){
-      this.setState({
-        which:++this.state.which
-      })
-    } else {
-      this.setState({
-        which:0
-      })
-    }
+    percentage = !this.state.forward ? percentage - multiplier : (this.state.which > 0 ? percentage - multiplier : 0 - slidesCount * multiplier);
+
+    var image = this.wrapperRef.current; 
+    //var singleImage = this.childRef.current.imageRefActive.current;
+
+    var tl = new TimelineMax({repeat:0});
+    tl.to( image , 2 , { top: `${percentage}%`, ease: 'Expo.easeOut' }); 
+    //var ntl = new TimelineMax({repeat:0});    
+    //ntl.to( singleImage, 1, {backgroundSize: '120%', ease: 'Expo.easeOut'}).to( singleImage, 1, {backgroundSize: '100%', ease: 'Expo.easeOut'});
+
+    console.log("NEXT", "percentage", percentage, "this.state.which", this.state.which, "slidesCount", slidesCount, "this.state.forward", this.state.forward); 
+
+    this.setState({
+      which: this.state.which > 0 ? --this.state.which : 0,
+      forward: true       
+    })
 
   }
 
   componentWillUnmount(){
     clearInterval(startCarouselInterval);
-    this.setState({ advance:true }) 
   }
 
   render() {
 
     var carouselLeftButton = (
-      <div style={{display:'flex',zIndex:9999,alignItems:'center',alignContent:'center',height:'100vh'}}>
+      <div style={{display:'flex', zIndex:9999, alignItems:'center', alignContent:'center', height:'100vh'}}>
       <button onClick={this.prevSlide.bind(this)} className="btn btn-primary">+</button>
       </div>)
 
     var carouselRightButton = (
-      <div style={{display:'flex',zIndex:9999,alignItems:'center',alignContent:'center',height:'100vh',marginLeft:'auto'}}>
+      <div style={{display:'flex', zIndex:9999, alignItems:'center', alignContent:'center', height:'100vh', marginLeft:'auto'}}>
       <button onClick={this.nextSlide.bind(this)} className="btn btn-primary">+</button>
       </div>)
 
-    var carouselImages =  this.props.arrayOfImages.map((image, i) =>{     
-      if(i === this.state.which){  
-        return(
-          <CarouselImage key={i} label={labels[i]} timeInBetween={this.props.timeInBetween} whichOne={i} src={image} src_prev={images[i-1]} advance={this.state.advance}/>
-        )   
-      }
+    var carouselImages =  this.props.arrayOfImages.map((image, i) =>{
+      return(
+        <CarouselImage ref={i => this.titles.push(i)}﻿  key={i} label={labels[i]} timeInBetween={this.props.timeInBetween} whichOne={i} src={image} position={i*100} />
+      )   
     })
 
     return (
       <div style={{display:'flex',flexDirection:'row',position:'relative'}}>
         {this.state.showButtons  || !this.props.auto ? carouselLeftButton : null  } 
-        <div>{carouselImages}</div>
+        <div ref={this.wrapperRef} style={{position:'absolute', bottom: 0, top: '0%', left: 0, right: 0, width: '100%', height: '100%' }}>{carouselImages}</div>
         {this.state.showButtons  || !this.props.auto ? carouselRightButton : null  } 
       </div>
     );
@@ -126,29 +139,14 @@ class CarouselImage extends React.Component {
     super(props);
     this.state = { imageStatus: null };
     this.imageRefActive = React.createRef();
-    this.imageRefPrev = React.createRef();    
   }
  
   _handleImageLoaded() {
     this.setState({ imageStatus: 'loaded' });
   }
 
-  _animateIn(){
-    var image = this.imageRefActive.current; 
-    var tl = new TimelineMax({repeat:0});
-    tl.to(image , 1 , {opacity:1, ease:Back.easeInOut }); 
-    //console.log(tl); 
-  }
-
-  _animateOut(){
-    var image = this.imageRefPrev.current; 
-    var tl = new TimelineMax({repeat:0});
-    tl.to(image , 1 , {opacity:0, ease:Back.easeInOut }); 
-    //console.log(tl);    
-  }  
-
   componentDidMount(){
-    this._animateIn();
+    //console.log(this.props.children, "child");
   }
 
   _handleImageErrored() {
@@ -156,27 +154,18 @@ class CarouselImage extends React.Component {
   }
 
   render() {
-    var src_prev = `${this.props.src_prev}`;
-    var srcTo_prev = ' no-repeat center center';
-    var srcToFull_prev = 'url(' +  src_prev + ')'+  srcTo_prev;
+    var pos =  `${this.props.position}`;
     var src = `${this.props.src}`;
     var srcTo = ' no-repeat center center';
     var srcToFull = 'url(' +  src + ')'+  srcTo;
-    console.log(srcToFull,"srcToFull");
-    console.log(src_prev,"srcToPrev");
+    //console.log(srcToFull,"srcToFull",pos,"POS");
     return (
-      <div style={{width: '100%', height: '100%',display:'flex',flex:1}}>
+      <div style={{width: '100%', height: '100%', display:'flex', flex:1, position: 'absolute'}}>
         <img ref={this.imageRefActive} 
-         style={{ zIndex:-1, opacity:0, background:srcToFull, backgroundSize: 'cover',position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, margin: 'auto', minWidth: '100%', minHeight: '100%'}}
+         style={{ zIndex:-1, opacity:1, background:srcToFull, backgroundSize: '100%', position: 'absolute', top: `${this.props.position}%`, left: 0, right: 0, bottom: 0, margin: 'auto', width: '50%', height: '50%'}}
           onLoad={this._handleImageLoaded.bind(this)}
           onError={this._handleImageErrored.bind(this)}
           />
-        <img ref={this.imageRefPrev} 
-         style={{ zIndex:-1, opacity:0, background:srcToFull_prev, backgroundSize: 'cover',position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, margin: 'auto', minWidth: '100%', minHeight: '100%'}}
-          onLoad={this._handleImageLoaded.bind(this)}
-          onError={this._handleImageErrored.bind(this)}
-          />          
-        <div style={{zIndex:999,padding:5,position:'absolute',height:30,left:0,bottom:0,width:'100%',textAlign:'center',margin:'auto',background:'black'}}><h1 style={{margin:0}}>{this.props.label}</h1></div>
       </div>
     );
   }
