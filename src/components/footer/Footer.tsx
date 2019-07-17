e27cae2a031e600de99c739dcb82895e660d1d42
gridig <igorabdul@gmail.com>
2019-07-17 20:41:56 +0200

    footer children

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,21 +1,28 @@
 import * as React from 'react';
+import TransitionLink from 'gatsby-plugin-transition-link'
 
 import s from './Footer.scss';
 
 interface IFooterProps {
-  logo: React.ReactNode;
+  children: React.ReactNode;
 }
 
-export const Footer = ({ logo }: IFooterProps) => (
+export const Footer = ({ children, className, delay }: IFooterProps) => (
   <div className={s.footer}>
     <div className={s.footer__col1}>
-      <h2>what</h2>
+      <TransitionLink  to="/" className={s.footer__col1} exit={{ length: 1 }} entry={{ delay: 0 }}>
+      	<h2 className={`${s.footer__col1} ${className}`}>{children}</h2>
+      </TransitionLink>
     </div>
-    <div className={s.footer__col2}>
-      <h2>what</h2> 
+    <div className={s.footer__col2}>      
+      <TransitionLink  to="/" className={s.footer__col2} exit={{ length: 1 }} entry={{ delay: 0 }}>
+      	<h2 className={`${s.footer__col2} ${className}`}>{children}</h2>
+      </TransitionLink>
     </div>
-    <div className={s.footer__col3}>
-      <h2>what</h2> 
+    <div className={s.footer__col3}>      
+      <TransitionLink  to="/" className={s.footer__col3} exit={{ length: 1 }} entry={{ delay: 0 }}>
+      	<h2 className={`${s.footer__col3} ${className}`}>{children}</h2>
+      </TransitionLink>
     </div>
   </div>
 );
