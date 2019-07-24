56a4f2cba2403f576b9ab245d522f65ede01bb95
gridig <igorabdul@gmail.com>
2019-07-24 10:58:59 +0200

    alena

--- src/pages/BakersAbout.tsx
+++ src/pages/BakersAbout.tsx
@@ -111,9 +111,11 @@ const BakersAbout = ({ data, className }) => {
         <Row>
             <div className="bakers_about__column_four">
 
-                <Split className="bakers_about__heading_four">Meet the team</Split>
+                <Split className="bakers_about__heading_five">Meet the team</Split>
 
-                <Paragraph className="bakers_about__paragraph_four">Alena Pelcova</Paragraph>
+                <Paragraph className="bakers_about__paragraph_five">Alena Pelcova</Paragraph>
+
+                <Paragraph className="bakers_about__paragraph_six">Imagine you have a family with thousands of relatives. This is exactly the family Alena takes care of. Future Bakery family comprising of twenty five thousands people from the crowd. People with great energy and ideas. We know very well that none of us is as smart as we all together. Also, that we are all creative. It’s enough to give impulse and it rolls off. This world is full of creativity, fresh and - for somebody - weird ideas and insights. Our work is to work well with this and give it all a life.</Paragraph>
 
 
             </div>
