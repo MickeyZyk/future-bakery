1cee863dddd101d67a754248445323e3fffb7ddf
Dalibor Ilić <dalibor.ilic.dado@gmail.com>
2019-08-11 18:48:37 +0200

    scrollbar

--- src/pages/crowderscontact.tsx
+++ src/pages/crowderscontact.tsx
@@ -7,7 +7,7 @@ import SVGiconReverse from 'components/svgiconreverse/SVGiconReverse';
 import ReactCursorPosition from 'react-cursor-position';
 import { Link } from 'components/link/Link';
 import { Row } from 'components/row/Row';
-
+import Scrollbar from 'react-smooth-scrollbar';
 
 import { TweenMax, TimelineMax, Power3} from "gsap";
 import { Tween } from 'react-gsap';
@@ -20,6 +20,7 @@ export default () => (
       <SVGicon className='bakery_contact_house' src='crowders_home.svg' /> 
 
 
+
       <TransitionState>
         {({ transitionStatus }) => {
           return (
@@ -29,24 +30,30 @@ export default () => (
             <Tween duration={2} 
             from={ ['entering'].includes(transitionStatus) ? false : { yPercent: 100, opacity: 1, ease: 'Power3.easeInOut' } } 
             to={ ['exiting'].includes(transitionStatus) ? { yPercent: -100, opacity: 1, ease: 'Power3.easeInOut' } : false  } >  
-
           
               <div className='fulscreen_slider'>
-                <AnimatedHeading className='bakery_contact_heading crowders_color'>Headline call to action for contact page.</AnimatedHeading>
-                <div className="bakery_contact_adress_block">
-                  <p className="text-bolder">The Future Bakery s.r.o.</p>
-                  <p>Opletalova 1013/59, Nové Mesto<br/>
-                  110 00 Praha 1<br/>
-                  IC: 24168858 DIC: CZ24168858</p>
-                  <p>&nbsp;</p>
-                  <p className="text-bolder">Máte zájem se na cokoliv zeptat?</p>
-                  <p>Neváhejte a kontaktujte nás.</p>
-                  <a className="contact-email color-black" href="mailto:info@futurebakery.cz">info@futurebakery.cz</a>
-                  <Row>                
-                    <Link className="bakery_contact_flip_button" crowders button arrow to={'/'}>START A PROJECT WITH US</Link>
-                  </Row>
-                </div>
-                <img src="../images/paper_plane.jpg" className='bakery_contact_image'/>
+
+                  <Scrollbar className="scrollbar" damping={0.1} renderByPixels={true} alwaysShowTracks={false} syncCallbacks={false}>      
+
+
+                  <AnimatedHeading className='bakery_contact_heading crowders_color'>Headline call to action for contact page.</AnimatedHeading>
+                  <div className="bakery_contact_adress_block">
+                    <p className="text-bolder">The Future Bakery s.r.o.</p>
+                    <p>Opletalova 1013/59, Nové Mesto<br/>
+                    110 00 Praha 1<br/>
+                    IC: 24168858 DIC: CZ24168858</p>
+                    <p>&nbsp;</p>
+                    <p className="text-bolder">Máte zájem se na cokoliv zeptat?</p>
+                    <p>Neváhejte a kontaktujte nás.</p>
+                    <a className="contact-email color-black" href="mailto:info@futurebakery.cz">info@futurebakery.cz</a>
+                    <Row>                
+                      <Link className="bakery_contact_flip_button" crowders button arrow to={'/'}>START A PROJECT WITH US</Link>
+                    </Row>
+                  </div>
+                  <img src="../images/paper_plane.jpg" className='bakery_contact_image'/>
+
+                </Scrollbar>
+                
               </div>
 
 
@@ -66,6 +73,10 @@ export default () => (
           )
         }}
       </TransitionState>
+
+
+
+
     </ReactCursorPosition>	    
   </>
 );
