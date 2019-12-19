130def87102f75a0feb5f164e1f45ca388e6cf36
Dalibor Ilić <dalibor.ilic.dado@gmail.com>
2019-12-19 10:05:31 +0100

    Update Slider.tsx

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -579,12 +579,18 @@ class Carousel extends React.Component {
         <div className="desktop_carousel" style={{display:'flex',flexDirection:'row',position:'relative', height: '100%'}}>
           <ReactCursorPosition className='fullscreen_cursor_position'>
 
+        <Location>
+          {({ location }) => (
+
             <If condition={!location.pathname.includes('crowders')}>
 
               <SVGicon className={`${'home_arrow'} ${this.state.animating ? 'home_arrow_current': ''}`} src='home_arrow.svg'  />
 
             </If>
 
+
+          )}
+        </Location>
 
             <div className='mask_wrapper_top' style={{left: 0, top: 'auto', position: 'absolute', right: 0, bottom: '7vw', height: '35vw', overflow: 'hidden'}}>
               <div ref={this.wrapperRef_top} className='mask_parent_top' 
