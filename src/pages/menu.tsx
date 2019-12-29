9673b89d3187ff288d0d4dbc6238ee8da01f67b3
Dalibor Ilić <dalibor.ilic.dado@gmail.com>
2019-12-29 18:42:48 +0100

    New Menu

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -17,6 +17,8 @@ export default class BakeryMenu extends React.Component {
   constructor(props) {
     super(props);
     this.unorderedList = React.createRef();
+    this.unorderedList2 = React.createRef();
+    this.unorderedList3 = React.createRef();    
     this.state = {
       isHovered: false,
       setHovered: false,
@@ -40,6 +42,18 @@ export default class BakeryMenu extends React.Component {
 
     var itemsTL = new TimelineMax();
     itemsTL.staggerFrom(listItems, 2, { yPercent: -200, opacity: 0, ease: 'Power3.easeInOut' }, .1, "+=0");
+
+    var uL2 = this.unorderedList2.current;
+    var listItems2 = uL2.querySelectorAll('.menu_item_wrapper');
+
+    var itemsTL2 = new TimelineMax();
+    itemsTL2.staggerFrom(listItems2, 2, { yPercent: -200, opacity: 0, ease: 'Power3.easeInOut' }, .1, "+=0");    
+
+    var uL3 = this.unorderedList3.current;
+    var listItems3 = uL3.querySelectorAll('.menu_item_wrapper');
+
+    var itemsTL3 = new TimelineMax();
+    itemsTL3.staggerFrom(listItems3, 2, { yPercent: -200, opacity: 0, ease: 'Power3.easeInOut' }, .1, "+=0");     
 
   }
 
@@ -69,74 +83,12 @@ export default class BakeryMenu extends React.Component {
 					  <Helmet title="Future Bakery" />
 							<div className='fullscreen mob_menu_wrapper'>
   
-						      <Helmet title="Bakery" />
-									<div className='menu_row one_third'>
-											<ul ref={this.unorderedList}>
-											  <li className='menu_item_wrapper'>
-												  <Link bakery
-													onMouseEnter={this.toggleHover}
-							      			onMouseLeave={this.toggleHover} 
-							      			className={ this.hovered ? 'hovered submenu_link' : 'submenu_link'}
-							      			name="About us" 
-							      			to="/bakery-about"
-				      						>
-							    					<ReactCursorPosition className='menu_cursor_position'> 						      				
-												      <SVGicon className='menu_crown' src='bakery_menu_crown.svg' /> 
-												      <SVGiconReverse  className='menu_baloons' src='bakery_menu_baloons.svg' /> 
-												      <SVGicon className='menu_temple' src='bakery_menu_temple.svg' /> 
-												    </ReactCursorPosition>				      						
-														<MenuHeading className='white_text'>About Us</MenuHeading>
-												  </Link>
-											  </li>
-											  <li className='menu_item_wrapper'>
-												  <Link bakery
-												  onMouseEnter={this.toggleHover}
-							      			onMouseLeave={this.toggleHover} 
-							      			className={ this.hovered ? 'hovered submenu_link' : 'submenu_link'}
-							      			name="Work" 
-							      			to="/bakery-work"
-						      				>
-							    					<ReactCursorPosition className='menu_cursor_position'> 						      				
-												      <SVGicon className='menu_saw' src='bakery_menu_saw.svg' /> 
-												      <SVGiconReverse  className='menu_drill' src='bakery_menu_drill.svg' /> 
-												      <SVGicon className='menu_paint' src='bakery_menu_paint.svg' />
-												    </ReactCursorPosition>												      
-														<MenuHeading className='white_text'>Work</MenuHeading>
+						      
 
-												  </Link>
-											  </li>
-											  <li className='menu_item_wrapper'>
-												  <Link bakery
-												  onMouseEnter={this.toggleHover}
-							      			onMouseLeave={this.toggleHover} 
-							      			className={ this.hovered ? 'hovered submenu_link' : 'submenu_link'}
-							      			name="Future Bakery Family" 
-							      			to="/family"
-						      				>
-														<MenuHeading className='white_text'>Future Bakery family</MenuHeading>												    
-												  </Link>
-											  </li>
-											  <li className='menu_item_wrapper'>
-												  <Link bakery
-												  onMouseEnter={this.toggleHover}
-							      			onMouseLeave={this.toggleHover} 
-							      			className={ this.hovered ? 'hovered submenu_link' : 'submenu_link'}
-							      			name="Contact" 
-							      			to="/bakerycontact"
-						      				>
-							    					<ReactCursorPosition className='menu_cursor_position'> 								      				
-												      <SVGicon className='menu_phone' src='bakery_menu_phone.svg' /> 
-												      <SVGiconReverse  className='menu_blah' src='bakery_menu_blah.svg' /> 
-												      <SVGicon className='menu_chat' src='bakery_menu_chat.svg' /> 						      				
-												    </ReactCursorPosition>						      				
-														<MenuHeading className='white_text'>Contact</MenuHeading>
-												  </Link>
-											  </li>
-										  </ul>
-								  </div>
+									<div className='menu_row'>
 
-									<div className='menu_row one_third'>
-											<ul ref={this.unorderedList}>
+
+											<ul className="one_third" ref={this.unorderedList}>
 											  <li className='menu_item_wrapper'>
 												  <Link bakery
 													onMouseEnter={this.toggleHover}
@@ -186,60 +138,106 @@ export default class BakeryMenu extends React.Component {
 												  </Link>
 											  </li>
 										  </ul>
-								  </div>
 
-									<div className='menu_row one_third'>
-											<ul ref={this.unorderedList}>
+
+											<ul className="one_third" ref={this.unorderedList2}>
 											  <li className='menu_item_wrapper'>
-												  <Link bakery
+												  <Link bakers
 													onMouseEnter={this.toggleHover}
 							      			onMouseLeave={this.toggleHover} 
 							      			className={ this.hovered ? 'hovered submenu_link' : 'submenu_link'}
 							      			name="About us" 
-							      			to="/bakery-about"
+							      			to="/bakers-about"
 				      						>
-			      						
 														<MenuHeading className='white_text'>About Us</MenuHeading>
 												  </Link>
 											  </li>
 											  <li className='menu_item_wrapper'>
-												  <Link bakery
+												  <Link bakers
 												  onMouseEnter={this.toggleHover}
 							      			onMouseLeave={this.toggleHover} 
 							      			className={ this.hovered ? 'hovered submenu_link' : 'submenu_link'}
-							      			name="Work" 
-							      			to="/bakery-work"
+							      			name="Work"
+							      			to="/bakers-work"
 						      				>
-											      
 														<MenuHeading className='white_text'>Work</MenuHeading>
-
 												  </Link>
 											  </li>
 											  <li className='menu_item_wrapper'>
-												  <Link bakery
+												  <Link bakers
 												  onMouseEnter={this.toggleHover}
 							      			onMouseLeave={this.toggleHover} 
 							      			className={ this.hovered ? 'hovered submenu_link' : 'submenu_link'}
 							      			name="Future Bakery Family" 
 							      			to="/family"
 						      				>
-														<MenuHeading className='white_text'>Future Bakery family</MenuHeading>												    
+														<MenuHeading className='white_text'>Future Bakery family</MenuHeading>
 												  </Link>
 											  </li>
 											  <li className='menu_item_wrapper'>
-												  <Link bakery
+												  <Link bakers
 												  onMouseEnter={this.toggleHover}
 							      			onMouseLeave={this.toggleHover} 
 							      			className={ this.hovered ? 'hovered submenu_link' : 'submenu_link'}
 							      			name="Contact" 
-							      			to="/bakerycontact"
+							      			to="/bakerscontact"
 						      				>
-					      				
 														<MenuHeading className='white_text'>Contact</MenuHeading>
 												  </Link>
 											  </li>
 										  </ul>
-								  </div>								  								  
+
+
+											<ul className="one_third" ref={this.unorderedList3}>
+											  <li className='menu_item_wrapper'>
+												  <Link crowders
+													onMouseEnter={this.toggleHover}
+							      			onMouseLeave={this.toggleHover} 
+							      			className={ this.hovered ? 'hovered submenu_link' : 'submenu_link'}
+							      			name="About us" 
+							      			to="/crowders-about"
+				      						>
+														<MenuHeading className='white_text'>About Us</MenuHeading>
+												  </Link>
+											  </li>
+											  <li className='menu_item_wrapper'>
+												  <Link crowders
+												  onMouseEnter={this.toggleHover}
+							      			onMouseLeave={this.toggleHover} 
+							      			className={ this.hovered ? 'hovered submenu_link' : 'submenu_link'}
+							      			name="Work"
+							      			to="/crowders-work"
+						      				>
+														<MenuHeading className='white_text'>Work</MenuHeading>
+												  </Link>
+											  </li>
+											  <li className='menu_item_wrapper'>
+												  <Link crowders
+												  onMouseEnter={this.toggleHover}
+							      			onMouseLeave={this.toggleHover} 
+							      			className={ this.hovered ? 'hovered submenu_link' : 'submenu_link'}
+							      			name="Future Bakery Family" 
+							      			to="/family"
+						      				>
+														<MenuHeading className='white_text'>Future Bakery family</MenuHeading>
+												  </Link>
+											  </li>
+											  <li className='menu_item_wrapper'>
+												  <Link crowders
+												  onMouseEnter={this.toggleHover}
+							      			onMouseLeave={this.toggleHover} 
+							      			className={ this.hovered ? 'hovered submenu_link' : 'submenu_link'}
+							      			name="Contact" 
+							      			to="/crowderscontact"
+						      				>
+														<MenuHeading className='white_text'>Contact</MenuHeading>
+												  </Link>
+											  </li>
+										  </ul>
+
+
+
+								  </div>			  
 
 							</div>
 
