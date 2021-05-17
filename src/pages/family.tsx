e36532a1fb1270302cde501406064157a621d759
Dalibor Ilić <dalibor.ilic.dado@gmail.com>
2021-05-17 13:34:22 +0200

    Update family.tsx

--- src/pages/family.tsx
+++ src/pages/family.tsx
@@ -73,19 +73,19 @@ export default ({ state }) => {
 										<div className="family_wrapper family_bakery_wrapper" onMouseEnter={toggleHover} onMouseLeave={toggleHover} style={ current==0 ? {opacity: 1, right: 0} : {opacity: 0, right: '89vw'} }>
 									      <SVGicon className='family_icon family_bakery_icon' src='family_bakery.svg' /> 
 									      <h3 className="family_subheading">Future Bakery</h3>
-									      <p className="family_paragraph">Fresh ideas and original content by and for your future consumers. (JE TO OK?)</p>	
+									      <p className="family_paragraph">Fresh ideas and original content by and for your future consumers.</p>	
 									      <Link className="family_link family_bakery_link" to={'/bakery'}>FIND OUT MORE</Link>							      
 										</div>
 										<div className=" family_wrapper family_bakers_wrapper" onMouseEnter={toggleHover} onMouseLeave={toggleHover} style={ current==1 ?  {opacity: 1, right: '89vw'} : {opacity: 0, right: '178vw'} }>
 									      <SVGicon className='family_icon family_bakers_icon' src='family_bakers.svg' />
 									      <h3 className="family_subheading">Future Bakers</h3>
-									      <p className="family_paragraph">Unleash your creativity and ideas. Join thousands of other creative souls who are eager to share ideas.(JE TO OK?)</p>
+									      <p className="family_paragraph">Unleash your creativity and ideas. Join thousands of other creative souls who are eager to share ideas.</p>
 									      <Link className="family_link family_bakers_link" to={'/bakers'}>FIND OUT MORE</Link>							      
 										</div>
 										<div className=" family_wrapper family_crowders_wrapper" onMouseEnter={toggleHover} onMouseLeave={toggleHover} style={ current==2 ?  {opacity: 1, right: '178vw'} : {opacity: 0, right: '267vw'} }>
 									      <SVGicon className='family_icon family_crowders_icon' src='family_crowders.svg' />
 									      <h3 className="family_subheading">Future Crowders</h3>
-									      <p className="family_paragraph">We create demand through seeking and developing cross-social topics. Communities building.(JE TO OK?)</p>								      
+									      <p className="family_paragraph">We create demand through seeking and developing cross-social topics. Communities building.</p>								      
 									      <Link className="family_link family_crowders_link" to={'/crowders'}>FIND OUT MORE</Link>							      
 										</div>
 									</div>         
